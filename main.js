e85b97307294e56d207f15775751ce1fc73b3f8a
Tyler Galvin <tylergalvin97@gmail.com>
2022-02-07 09:31:31 -0500

    final calulator

---
+++ main.js
@@ -0,0 +1,85 @@
+function function7() {
+   let inputF = document.getElementById("myinput");
+   inputF.value =inputF.value + "7";
+}
+
+function function8() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "8";
+ }
+
+ function function9() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "9";
+ }
+
+ function functionDivide() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "/";
+ }
+
+ function function4() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "4";
+ }
+
+ function function5() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "5";
+ }
+
+ function function6() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "6";
+ }
+
+ function functionX() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "*";
+ }
+
+ function function1() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "1";
+ }
+
+ function function2() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "2";
+ }
+
+ function function3() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "3";
+ }
+
+ function functionMinus() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "-";
+ }
+
+ function function0() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "0";
+ }
+
+ function functionPeriod() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + ".";
+ }
+
+ function functionEqual() {
+    let inputF = document.getElementById("myinput");
+    let answer = eval(inputF.value);
+    inputF.value = answer
+ }
+
+ function functionPlus() {
+    let inputF = document.getElementById("myinput");
+    inputF.value =inputF.value + "+";
+ }
+
+ function functionClear() {
+    let inputF = document.getElementById("myinput");
+    inputF.value = "";
+ }
